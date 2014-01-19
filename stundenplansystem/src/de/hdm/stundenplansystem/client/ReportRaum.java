81b3b36e39c25649fb2f72b86723b61a2b9fb40b
Jennifer <Jennifer@192.168.1.15>
2014-01-19 17:39:35 +0100

    oisdn

--- stundenplansystem/src/de/hdm/stundenplansystem/client/ReportRaum.java
+++ stundenplansystem/src/de/hdm/stundenplansystem/client/ReportRaum.java
@@ -48,47 +48,47 @@ public class ReportRaum extends Content {
 			  	}
 		  }
 		  });
-		getData();
+//		getData();
 	}
 
 	public void setTvm(NavTreeViewModel tvm) {
 		this.tvm = tvm;
 	}
 	
-		public void getData(){	
-			this.add(flexRaum);
-			
-			reportSvc.createRaumbelungsReport(r, new AsyncCallback<RaumbelegungsReport>() {
-
-				public void onSuccess(RaumbelegungsReport result){
-					if (result != null){
-				
-						flexRaum.setText(0, 0, "Zeit");
-						flexRaum.setText(1, 0, "08:15 - 09:45 Uhr");
-						flexRaum.setText(2, 0, "10:00 - 11:30 Uhr");
-						flexRaum.setText(3, 0, "11:45 - 13:15 Uhr");
-						flexRaum.setText(4, 0, "13:25 - 14:15 Uhr");
-						flexRaum.setText(5, 0, "14:15 - 15:45 Uhr");
-						flexRaum.setText(6, 0, "16:00 - 17:30 Uhr");
-						flexRaum.setText(7, 0, "17:45 - 19:15 Uhr");
-						flexRaum.setText(0, 1, "Montag");
-						flexRaum.setText(0, 2, "Dienstag");
-						flexRaum.setText(0, 3, "Mittowch");
-						flexRaum.setText(0, 4, "Donnerstag");
-						flexRaum.setText(0, 5, "Freitag");
-						flexRaum.setText(0, 6, "Samstag");
-				
-						int row = 1; 
-						for (int i=0; i<result..size(); i++) {
-							flexRaum.setText(row, 1, String.valueOf(result.get(i).getLehrveranstaltungId())); 
-						}
-					}
-				}
-
-				@Override
-				public void onFailure(Throwable caught) {					
-				}
-			});
-		}
+//		public void getData(){	
+//			this.add(flexRaum);
+//			
+//			reportSvc.createRaumbelungsReport(r, new AsyncCallback<RaumbelegungsReport>() {
+//
+//				public void onSuccess(RaumbelegungsReport result){
+//					if (result != null){
+//				
+//						flexRaum.setText(0, 0, "Zeit");
+//						flexRaum.setText(1, 0, "08:15 - 09:45 Uhr");
+//						flexRaum.setText(2, 0, "10:00 - 11:30 Uhr");
+//						flexRaum.setText(3, 0, "11:45 - 13:15 Uhr");
+//						flexRaum.setText(4, 0, "13:25 - 14:15 Uhr");
+//						flexRaum.setText(5, 0, "14:15 - 15:45 Uhr");
+//						flexRaum.setText(6, 0, "16:00 - 17:30 Uhr");
+//						flexRaum.setText(7, 0, "17:45 - 19:15 Uhr");
+//						flexRaum.setText(0, 1, "Montag");
+//						flexRaum.setText(0, 2, "Dienstag");
+//						flexRaum.setText(0, 3, "Mittowch");
+//						flexRaum.setText(0, 4, "Donnerstag");
+//						flexRaum.setText(0, 5, "Freitag");
+//						flexRaum.setText(0, 6, "Samstag");
+//				
+//					int row = 1; 
+//						for (int i=0; i<result..size(); i++) {
+//							flexRaum.setText(row, 1, String.valueOf(result.get(i).getLehrveranstaltungId())); 
+//						}
+//					}
+//				}
+//
+//				@Override
+//				public void onFailure(Throwable caught) {					
+//				}
+//			});
+//		} 
 
 }

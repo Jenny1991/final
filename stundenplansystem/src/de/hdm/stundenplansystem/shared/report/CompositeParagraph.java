ad08a3e9c55292b8778b9763818057eb44918c28
DominikHolz <dominik.j.holz@gmail.com>
2014-01-20 19:34:17 +0100

    ....

--- stundenplansystem/src/de/hdm/stundenplansystem/shared/report/CompositeParagraph.java
+++ stundenplansystem/src/de/hdm/stundenplansystem/shared/report/CompositeParagraph.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 import java.util.Vector;
 
 /**
- * Diese Klasse stellt eine Menge einzelner AbsÃ¤tze (
+ * Diese Klasse stellt eine Menge einzelner Abs??tze (
  * <code>SimpleParagraph</code>-Objekte) dar. Diese werden als Unterabschnitte
  * in einem <code>Vector</code> abgelegt verwaltet.
  * 
@@ -23,9 +23,9 @@ public class CompositeParagraph extends Paragraph implements Serializable {
   private Vector<SimpleParagraph> subParagraphs = new Vector<SimpleParagraph>();
 
   /**
-   * Einen Unterabschnitt hinzufÃ¼gen.
+   * Einen Unterabschnitt hinzuf??gen.
    * 
-   * @param p der hinzuzufÃ¼gende Unterabschnitt.
+   * @param p der hinzuzuf??gende Unterabschnitt.
    */
   public void addSubParagraph(SimpleParagraph p) {
     this.subParagraphs.addElement(p);
@@ -41,9 +41,9 @@ public class CompositeParagraph extends Paragraph implements Serializable {
   }
 
   /**
-   * Auslesen sÃ¤mtlicher Unterabschnitte.
+   * Auslesen s??mtlicher Unterabschnitte.
    * 
-   * @return <code>Vector</code>, der sÃ¤mtliche Unterabschnitte enthÃ¤lt.
+   * @return <code>Vector</code>, der s??mtliche Unterabschnitte enth??lt.
    */
   public Vector<SimpleParagraph> getSubParagraphs() {
     return this.subParagraphs;
@@ -61,10 +61,10 @@ public class CompositeParagraph extends Paragraph implements Serializable {
   /**
    * Auslesen eines einzelnen Unterabschnitts.
    * 
-   * @param i der Index des gewÃ¼nschten Unterabschnitts (0 <= i <n), mit n =
+   * @param i der Index des gew??nschten Unterabschnitts (0 <= i <n), mit n =
    *          Anzahl der Unterabschnitte.
    * 
-   * @return der gewÃ¼nschte Unterabschnitt.
+   * @return der gew??nschte Unterabschnitt.
    */
   public SimpleParagraph getParagraphAt(int i) {
     return this.subParagraphs.elementAt(i);
@@ -76,24 +76,24 @@ public class CompositeParagraph extends Paragraph implements Serializable {
    */
   public String toString() {
     /*
-     * Wir legen einen leeren Buffer an, in den wir sukzessive sÃ¤mtliche
-     * String-ReprÃ¤sentationen der Unterabschnitte eintragen.
+     * Wir legen einen leeren Buffer an, in den wir sukzessive s??mtliche
+     * String-Repr??sentationen der Unterabschnitte eintragen.
      */
     StringBuffer result = new StringBuffer();
 
-    // Schleife Ã¼ber alle Unterabschnitte
+    // Schleife ??ber alle Unterabschnitte
     for (int i = 0; i < this.subParagraphs.size(); i++) {
       SimpleParagraph p = this.subParagraphs.elementAt(i);
 
       /*
-       * den jew. Unterabschnitt in einen String wandeln und an den Buffer hÃ¤ngen.
+       * den jew. Unterabschnitt in einen String wandeln und an den Buffer h??ngen.
        */
       result.append(p.toString() + "\n");
     }
 
     /*
-     * SchlieÃŸlich wird der Buffer in einen String umgewandelt und
-     * zurÃ¼ckgegeben.
+     * Schlie??lich wird der Buffer in einen String umgewandelt und
+     * zur??ckgegeben.
      */
     return result.toString();
   }

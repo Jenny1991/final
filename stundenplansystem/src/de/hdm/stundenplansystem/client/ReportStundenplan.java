e32f1a05cc21dc84d4f48b86ed68bdf53405ac5a
Jennifer <Jennifer@G62Jenny>
2014-01-21 14:47:00 +0100

    ReportStundenplan

--- stundenplansystem/src/de/hdm/stundenplansystem/client/ReportStundenplan.java
+++ stundenplansystem/src/de/hdm/stundenplansystem/client/ReportStundenplan.java
@@ -13,16 +13,25 @@ import com.google.gwt.user.client.ui.ButtonBase;
 import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.ListBox;
+import com.google.gwt.user.client.ui.Panel;
 import com.google.gwt.user.client.ui.RadioButton;
+import com.google.gwt.user.client.ui.VerticalPanel;
 
 import de.hdm.stundenplansystem.client.Content;
 import de.hdm.stundenplansystem.shared.*;
+import de.hdm.stundenplansystem.shared.bo.Dozent;
 import de.hdm.stundenplansystem.shared.bo.Studiengang;
 import de.hdm.stundenplansystem.shared.bo.Stundenplan;
 import de.hdm.stundenplansystem.shared.bo.Semesterverband;
 import de.hdm.stundenplansystem.shared.report.*;
 
 public class ReportStundenplan extends Content {
+	
+	Vector<Semesterverband> svContainer = null;
+	Vector<Stundenplan> spContainer = null;
+	Vector<Studiengang> sgContainer = null;
+
+
 	/**
 	 * Aufbau der Seite, um den Stundenplan für Studenten anzuzeigen
 	 */
@@ -32,7 +41,9 @@ public class ReportStundenplan extends Content {
 	final FlexTable flexSv = new FlexTable();
 	final ListBox libstundenplan = new ListBox();
 	final ListBox libsemverband = new ListBox();
+	final ListBox libstudiengang = new ListBox();
 	final Button anzeigen = new Button("Stundenplan anzeigen");
+	final VerticalPanel neuesPanel = new VerticalPanel(); 
 
 	final VerwaltungsklasseAsync verwaltungsSvc = GWT.create(Verwaltungsklasse.class);
 	final ReportGeneratorAsync reportSvc = GWT.create(ReportGenerator.class);
@@ -44,68 +55,74 @@ public class ReportStundenplan extends Content {
 	public void onLoad() {
 		
 		this.add(ueberschrift);	
-		this.add(libsemverband);
 		this.add(libstundenplan);
+		this.add(libstudiengang);
+		this.add(libsemverband);
 		this.add(anzeigen);
 		setTvm(tvm);
 		
 		libstundenplan.clear();
-		  verwaltungsSvc.getAllStundenplaene(new AsyncCallback<Vector<Stundenplan>>() {
+/**		  verwaltungsSvc.getStundenplaeneBySemesterverband(new AsyncCallback<Vector<Stundenplan>>() {
 			  public void onFailure(Throwable T){
 				  
 			  }
 			  
 			  public void onSuccess(Vector<Stundenplan> stundenplaene){
+				spContainer = stundenplaene;
 			  	for (Stundenplan sp : stundenplaene){
 			  		libstundenplan.addItem(sp.getStudienhalbjahr(), String.valueOf(sp.getId()));
 			  	}
 		  }
 		  });
+		  
+		  libstudiengang.clear();
+		  verwaltungsSvc.getAllStudiengaenge(new AsyncCallback<Vector<Studiengang>>() {
+			  public void onFailure(Throwable T){
+				  
+			  }
+			  
+			  public void onSuccess(Vector<Studiengang> studiengang){
+				sgContainer = studiengang;
+			  	for (Studiengang sg : studiengang){
+			  		libstudiengang.addItem(sg.getBezeichnung(), String.valueOf(sg.getId()));
+			  	}
+		  }
+	}); 
 		
 		  libsemverband.clear();
-		  verwaltungsSvc.getAllSemesterverbaende(new AsyncCallback<Vector<Semesterverband>>() {
+		  verwaltungsSvc.getSemesterverbaendeByStudiengang(new AsyncCallback<Vector<Semesterverband>>() {
 			  public void onFailure(Throwable T){
 				  
 			  }
 			  
 			  public void onSuccess(Vector<Semesterverband> semesterverband){
+				svContainer = semesterverband;
 			  	for (Semesterverband sv : semesterverband){
 			  		libsemverband.addItem(sv.getJahrgang(), String.valueOf(sv.getId()));
 //			  		libsemverband.addItem(String.valueOf(sv.getSemester())); 
-//			  		libsemverband.addItem(sg.getBezeichnung(), String.valueOf(sg.getId()));
 			  	}
 		  }
-		  });
-	}
+	}); */
 		  
-//		  anzeigen.addClickHandler(new ClickHandler() {
-//			  public void onClick(ClickEvent event) {				 
-//
-//				  reportSvc.createStundenplanSemesterverbandReport(sv, new AsyncCallback<StundenplanSemesterverbandReport>() {
-//		
+		  anzeigen.addClickHandler(new ClickHandler() {
+			  public void onClick(ClickEvent event) {				 
+
+				  reportSvc.createStundenplanSemesterverbandReport(svContainer.elementAt(libsemverband.getSelectedIndex()).getId(), new AsyncCallback<StundenplanSemesterverbandReport>() {
+		
 					  public void onSuccess(StundenplanSemesterverbandReport result){
 						  
-						  HTML plan = new HTML(result);
-						  
+//						  HTML plan = new HTML(result);
+//						  neuesPanel.add(plan);
 					  }
-//						 return getReportText();
-//					  }
-//			
-//					  @Override
-//					  public void onFailure (Throwable caught) {
-//					  }
-//				  });
-//			  }
-//		  });
-//		
-//		  public ReportStundenplan(){
-//			  HTML feld = new HTML(); 
-//			  feld.
-//			  this.add(feld);
-//		  }
-//		  
-//			  konstruktor mit html und ergebnisstring;
-
+			
+					  @Override
+					  public void onFailure (Throwable caught) {
+					  }
+				  });
+			  }
+		  });
+		  }
+		  
 	public void setTvm(NavTreeViewModel tvm) {
 		this.tvm = tvm;
 	}

89bb48abf1e40796a15ca21cbacfcbe048a9ad0e
Verena92 <vh031@hdm-stuttgart.de>
2014-01-21 22:11:50 +0100

    test sg bei sv in tree

--- stundenplansystem/src/de/hdm/stundenplansystem/client/SemesterverbandCell.java
+++ stundenplansystem/src/de/hdm/stundenplansystem/client/SemesterverbandCell.java
@@ -1,9 +1,13 @@
 package de.hdm.stundenplansystem.client;
 
 import com.google.gwt.cell.client.AbstractCell;
-
+import com.google.gwt.core.shared.GWT;
 import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.ibm.icu.util.BytesTrie.Result;
 
+import de.hdm.stundenplansystem.shared.Verwaltungsklasse;
+import de.hdm.stundenplansystem.shared.VerwaltungsklasseAsync;
 import de.hdm.stundenplansystem.shared.bo.*;
 
 /**
@@ -11,22 +15,43 @@ import de.hdm.stundenplansystem.shared.bo.*;
  *
  */
 public class SemesterverbandCell extends AbstractCell<Semesterverband> {
-	@Override
-    public void render(Context context, Semesterverband value, SafeHtmlBuilder sb) {
-      // Value can be null, so do a null check..
-      if (value == null) {
-        return;
-      }
+	
+	public void render(Context context, final Semesterverband value, final SafeHtmlBuilder sb) {
+	      // Value can be null, so do a null check..
+	      if (value == null) {
+	        return;
+	      }
+	      
+        final VerwaltungsklasseAsync verwaltungsSvc = GWT.create(Verwaltungsklasse.class);
+			verwaltungsSvc.getStudiengangById(value.getStudiengangId(), new AsyncCallback<Studiengang>() {
+				@Override
+				  public void onFailure (Throwable caught) {
+				  }
 
-      sb.appendHtmlConstant("<div>");
-      //sb.appendEscaped(value.getBezeichnung());
-      sb.appendHtmlConstant(", ");
-      sb.append(value.getSemester());
-      sb.appendHtmlConstant(", ");
-      sb.append(value.getStudierendenAnzahl());
-      sb.appendHtmlConstant(", ");
-      sb.appendEscaped(value.getJahrgang());
-      sb.appendHtmlConstant("</div>");
-    }
+				@Override
+				public void onSuccess(Studiengang result) {
+				    sb.appendHtmlConstant("<div>");
+					sb.appendEscapedLines(result.getBezeichnung());
+				}
+			});
+			
+			sb.append(value.getStudiengangId());
+			sb.appendHtmlConstant(", ");
+		    sb.append(value.getSemester());
+		    sb.appendHtmlConstant(", ");
+		    sb.append(value.getStudierendenAnzahl());
+		    sb.appendHtmlConstant(", ");
+		    sb.appendEscaped(value.getJahrgang());
+		    sb.appendHtmlConstant("</div>");     
+				      
+						
+		}
+
 
 }
+		
+	
+
+
+
+

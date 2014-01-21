03d547742b954a3b7fd044fb92ca376c8f8520c5
Jennifer <Jennifer@G62Jenny>
2014-01-21 18:36:51 +0100

    nochmal

--- stundenplansystem/src/de/hdm/stundenplansystem/client/SemesterverbandForm.java
+++ stundenplansystem/src/de/hdm/stundenplansystem/client/SemesterverbandForm.java
@@ -130,7 +130,6 @@ public class SemesterverbandForm extends Content {
 				  if (allFilled == true) { 
 					  shownSv.setJahrgang(tbjahrgang.getText().trim());
 					  shownSv.setStudiengangId(sgContainer.elementAt(libstudiengang.getSelectedIndex()-1).getId());
-			//		  shownSv.setStudiengangId(Integer.valueOf(libstudiengang.getValue(libstudiengang.getSelectedIndex())));
 					  shownSv.setStudierendenAnzahl(Integer.valueOf(tbanzahl.getValue()));
 					  shownSv.setSemester(Integer.valueOf(tbsemester.getValue().trim()));
 	

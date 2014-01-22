8260fbdf2076e11a806d6b7c8e5bf180f2ca9d8f
Markus <markusschmieder@gmx.de>
2014-01-22 11:35:34 +0100

    odsn

--- stundenplansystem/src/de/hdm/stundenplansystem/client/SemesterverbandCell.java
+++ stundenplansystem/src/de/hdm/stundenplansystem/client/SemesterverbandCell.java
@@ -1,5 +1,7 @@
 package de.hdm.stundenplansystem.client;
 
+import java.util.Vector;
+
 import com.google.gwt.cell.client.AbstractCell;
 import com.google.gwt.core.shared.GWT;
 import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
@@ -16,13 +18,15 @@ import de.hdm.stundenplansystem.shared.bo.*;
  */
 public class SemesterverbandCell extends AbstractCell<Semesterverband> {
 	
+	String bezeichnung = null;
+    final VerwaltungsklasseAsync verwaltungsSvc = GWT.create(Verwaltungsklasse.class);
+
 	public void render(Context context, final Semesterverband value, final SafeHtmlBuilder sb) {
 	      // Value can be null, so do a null check..
 	      if (value == null) {
 	        return;
 	      }
 	      
-        final VerwaltungsklasseAsync verwaltungsSvc = GWT.create(Verwaltungsklasse.class);
 			verwaltungsSvc.getStudiengangById(value.getStudiengangId(), new AsyncCallback<Studiengang>() {
 				@Override
 				  public void onFailure (Throwable caught) {
@@ -30,12 +34,12 @@ public class SemesterverbandCell extends AbstractCell<Semesterverband> {
 
 				@Override
 				public void onSuccess(Studiengang result) {
-				    sb.appendHtmlConstant("<div>");
-					sb.appendEscapedLines(result.getBezeichnung());
+					bezeichnung = result.getBezeichnung();
 				}
 			});
 			
-			sb.append(value.getStudiengangId());
+		    sb.appendHtmlConstant("<div>");     
+			sb.appendEscapedLines(bezeichnung);
 			sb.appendHtmlConstant(", ");
 		    sb.append(value.getSemester());
 		    sb.appendHtmlConstant(", ");
@@ -46,8 +50,6 @@ public class SemesterverbandCell extends AbstractCell<Semesterverband> {
 				      
 						
 		}
-
-
 }
 		
 	

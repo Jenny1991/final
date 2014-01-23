fc9eef06c4a1a27a72f90fa5ecf3f649e593c1d1
Markus <markusschmieder@gmx.de>
2014-01-23 13:18:56 +0100

    svmapper

--- stundenplansystem/src/de/hdm/stundenplansystem/server/db/SemesterverbandMapper.java
+++ stundenplansystem/src/de/hdm/stundenplansystem/server/db/SemesterverbandMapper.java
@@ -134,7 +134,7 @@ public class SemesterverbandMapper {
         s.setStudierendenAnzahl(rs.getInt("studierendenAnzahl"));
         s.setJahrgang(rs.getString("jahrgang"));
         s.setStudiengangId(rs.getInt("studiengangid"));
-        s.setKuerzel("kuerzel");
+        s.setKuerzel(rs.getString("kuerzel"));
 
         // Hinzufügen des neuen Objekts zum Ergebnisvektor
         result.addElement(s);

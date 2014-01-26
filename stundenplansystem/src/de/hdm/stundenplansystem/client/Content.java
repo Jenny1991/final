1505b32ca7752b979b7fa4718299056920e2229c
Jennifer <Jennifer@192.168.1.15>
2014-01-26 13:43:57 +0100

    Content beschrieben

--- stundenplansystem/src/de/hdm/stundenplansystem/client/Content.java
+++ stundenplansystem/src/de/hdm/stundenplansystem/client/Content.java
@@ -5,7 +5,7 @@ import com.google.gwt.user.client.ui.VerticalPanel;
 /**
  * Durch diese Klasse werden die Gui Klassen imVerticalPanel angeordnet
  * 
- * @author V. Hofmannn 
+ * @author V. Hofmannn
  * @version 1.0
  */
 public class Content extends VerticalPanel {
